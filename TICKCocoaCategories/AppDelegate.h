2d8c0fdcc00e9fa42728469cbef4347985e92385
tickCoder <tickCoder@users.noreply.github.com>
2016-09-10 16:34:57 +0800

    增加工程

---
+++ TICKCocoaCategories/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TICKCocoaCategories
+//
+//  Created by Claris on 2016.09.10.
+//  Copyright © 2016 Claris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TICKCocoaCategories/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TICKCocoaCategories
+//
+//  Created by Claris on 2016.09.10.
+//  Copyright © 2016 Claris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
